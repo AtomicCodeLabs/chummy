3b6ae1f9999723df3c53898088f83130b30c1ac6
Alex Kim <alexgkim205@gmail.com>
2020-11-15 01:05:12 -0800

    ci: add decrypt key script

---
+++ scripts/decrypt_env.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+
+# --batch to prevent interactive command
+# --yes to assume "yes" for questions
+gpg --quiet --batch --yes --decrypt --passphrase="$ENV_PRODUCTION_SECRET_KEY" \
+--output $HOME/.env.production env.production.gpg
