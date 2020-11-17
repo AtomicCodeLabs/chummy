86ed275d51ba88c1e1c261d58d86ba1ad64a3848
Alex Kim <alexgkim205@gmail.com>
2020-11-16 22:19:37 -0800

    refactor: removed website files from master branch

--- website/.cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-firebase/gatsby-browser.js'),
-      options: {"plugins":[],"features":{"auth":true,"database":false,"firestore":true,"storage":false,"messaging":false,"functions":false,"performance":false},"credentials":{"apiKey":"<YOUR_FIREBASE_API_KEY>","authDomain":"<YOUR_FIREBASE_AUTH_DOMAIN>","databaseURL":"<YOUR_FIREBASE_DATABASE_URL>","projectId":"<YOUR_FIREBASE_PROJECT_ID>","storageBucket":"<YOUR_FIREBASE_STORAGE_BUCKET>","messagingSenderId":"<YOUR_FIREBASE_MESSAGING_SENDER_ID>","appId":"<YOUR_FIREBASE_APP_ID>"}},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
