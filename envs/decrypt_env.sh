ef8abe981b7dca0533d438f7f92c4d278be13ce8
Alex Kim <alexgkim205@gmail.com>
2020-11-19 00:06:55 -0800

    fix: env path again

--- envs/decrypt_env.sh
+++ envs/decrypt_env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
-pwd
+
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
 gpg --quiet --batch --yes --decrypt --passphrase="$ENV_PRODUCTION_SECRET_KEY" \
---output ./.env.production ./env.production.gpg
+--output ../envs/.env.production ../envs/env.production.gpg
