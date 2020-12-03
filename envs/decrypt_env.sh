d671f83a72f5ace38ccc8741fc17f29da5705a98
Alex Kim <alexgkim205@gmail.com>
2020-12-03 02:23:08 -0800

    ci: try sudo?

--- envs/decrypt_env.sh
+++ envs/decrypt_env.sh
@@ -2,8 +2,5 @@
 
 # --batch to prevent interactive command
 # --yes to assume "yes" for questions
-GNUPGHOME=$(mktemp -d $HOME/.gnupgXXXXXX)
-export GNUPGHOME
-gpg --quiet --batch --yes --decrypt --passphrase="$ENV_PRODUCTION_SECRET_KEY" \
+sudo gpg --quiet --batch --yes --decrypt --passphrase="$ENV_PRODUCTION_SECRET_KEY" \
 --output .env.production env.production.gpg
-rm -rfi $GNUPGHOME
