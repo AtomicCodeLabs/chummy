946852438887efb63991c2a14c1ff3902ec532ba
Alex Kim <alexgkim205@gmail.com>
2020-12-22 18:01:23 -0500

    refactor: removed website-backup

--- website-backup/.cache/api-runner-browser-plugins.js
+++
@@ -1,7 +0,0 @@
-module.exports = [{
-      plugin: require('../node_modules/gatsby-plugin-firebase/gatsby-browser.js'),
-      options: {"plugins":[],"features":{"auth":true,"database":false,"firestore":true,"storage":false,"messaging":false,"functions":false,"performance":false},"credentials":{}},
-    },{
-      plugin: require('../gatsby-browser.js'),
-      options: {"plugins":[]},
-    }]
