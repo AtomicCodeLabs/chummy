3a971d5dc0007cabe455b2e366eff7a1ce05e263
Alex Kim <alexgkim205@gmail.com>
2020-12-22 20:57:15 -0500

    chore: fix prod promotion sh

--- extension/scripts/promote.prod.sh
+++ extension/scripts/promote.prod.sh
@@ -3,14 +3,14 @@ yarn checkout:gamma
 git push -u origin extension/gamma
 
 # Checkout target branch
-yarn checkout:gamma
+yarn checkout:prod
 
 # Merge
-git merge extension/dev
+git merge extension/gamma
 
 # Push target branch
 amplify push
-git push -u origin extension/gamma
+git push -u origin extension/prod
 
 # Return to dev branch
 yarn checkout:dev
