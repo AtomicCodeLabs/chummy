1b3f35f9de22c5fea6eb0de7b7a199cf55712073
Alex Kim <alexgkim205@gmail.com>
2020-12-23 02:52:46 -0500

    chore: change extension to website in scripts

--- website/scripts/promote.gamma.sh
+++ website/scripts/promote.gamma.sh
@@ -2,20 +2,20 @@
 yarn checkout:dev
 git add -A
 HUSKY_SKIP_HOOKS=1 git commit -m "chore(pre-promote): push dev"
-git push -u origin extension/dev
+git push -u origin website/dev
 
 # Checkout target branch
 yarn checkout:gamma
 git add -A
 HUSKY_SKIP_HOOKS=1 git commit -m "chore(pre-promote): push gamma"
-git push -u origin extension/gamma
+git push -u origin website/gamma
 
 # Merge
-HUSKY_SKIP_HOOKS=1 git merge --no-edit -s recursive -X ours extension/dev
+HUSKY_SKIP_HOOKS=1 git merge --no-edit -s recursive -X ours website/dev
 
 # Push target branch
 amplify push
-git push -u origin extension/gamma
+git push -u origin website/gamma
 
 # Return to dev branch
 yarn checkout:dev
