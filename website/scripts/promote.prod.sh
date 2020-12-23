ef521d20414275d20e4b6aba5f6a952bb15d2ed8
Alex Kim <alexgkim205@gmail.com>
2020-12-23 02:49:30 -0500

    chore: adding scripts

---
+++ website/scripts/promote.prod.sh
@@ -0,0 +1,21 @@
+# Checkout source branch first
+yarn checkout:gamma
+git add -A
+HUSKY_SKIP_HOOKS=1 git commit -m "chore(pre-promote): push gamma"
+git push -u origin extension/gamma
+
+# Checkout target branch
+yarn checkout:prod
+git add -A
+HUSKY_SKIP_HOOKS=1 git commit -m "chore(pre-promote): push prod"
+git push -u origin extension/prod
+
+# Merge
+HUSKY_SKIP_HOOKS=1 git merge --no-edit -s recursive -X ours extension/gamma
+
+# Push target branch
+amplify push
+git push -u origin extension/prod
+
+# Return to dev branch
+yarn checkout:dev
