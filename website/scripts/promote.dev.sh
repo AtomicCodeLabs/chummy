930e6a7a6bc2296710555c1ee9c894d1267ab3a5
Alex Kim <alexgkim205@gmail.com>
2020-12-23 20:04:29 -0500

    ci: add script

---
+++ website/scripts/promote.dev.sh
@@ -0,0 +1,8 @@
+# Checkout source branch first
+yarn checkout:dev
+git add -A
+HUSKY_SKIP_HOOKS=1 git commit -m "chore(pre-promote): push dev"
+git push -u origin website/dev
+
+# Push backend
+amplify push
