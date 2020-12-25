9a49dd5519342ca9e2900a6482fa793f2fe8dea2
Alex Kim <alexgkim205@gmail.com>
2020-12-25 16:57:56 -0500

    ci: separate asset buckets for prod and gamma

--- extension/scripts/prepublish.sh
+++ extension/scripts/prepublish.sh
@@ -1,9 +1,10 @@
 cd dist
 
 VERSION=$(cat ".version")
+STAGE=$1
 
 # Pushing to
 for filename in web/{popup_$VERSION,background.dao_$VERSION}*.js; do
-  echo "Pushing $filename to s3://chummy-assets/$VERSION/"
-  aws s3 cp $filename s3://chummy-assets/$VERSION/
+  echo "Pushing $filename to s3://chummy-assets-$STAGE/$VERSION/"
+  aws s3 cp $filename s3://chummy-assets-$STAGE/$VERSION/
 done
