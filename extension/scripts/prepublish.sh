ce0ba591ac7749551129837b6fba53cc69aa9c89
Alex Kim <alexgkim205@gmail.com>
2020-12-25 16:31:49 -0500

    ci: fixing upload filepath

--- extension/scripts/prepublish.sh
+++ extension/scripts/prepublish.sh
@@ -4,6 +4,6 @@ VERSION=$(cat ".version")
 
 # Pushing to
 for filename in web/{popup_$VERSION,background.dao_$VERSION}*.js; do
-  echo "Pushing $filename to s3://chummy-assets/$VERSION"
-  # aws s3 cp $filename s3://chummy-assets/$VERSION/
+  echo "Pushing $filename to s3://chummy-assets/$VERSION/"
+  aws s3 cp $filename s3://chummy-assets/$VERSION/
 done
--- extension/scripts/publish.sh
+++ extension/scripts/publish.sh
@@ -3,7 +3,7 @@ cd dist
 VERSION=$(cat ".version")
 
 zip -r dist_$VERSION.moz.zip moz
-aws s3 cp dist_$VERSION.moz.zip s3://chummy-assets/$VERSiON/
+aws s3 cp dist_$VERSION.moz.zip s3://chummy-assets/$VERSION/
 
 zip -r dist_$VERSION.web.zip web
-aws s3 cp dist_$VERSION.web.zip s3://chummy-assets/$VERSiON/
+aws s3 cp dist_$VERSION.web.zip s3://chummy-assets/$VERSION/
