452a7852e19046a60abddce2ee839fb60b17f965
Alex Kim <alexgkim205@gmail.com>
2020-12-29 05:07:22 -0500

    feat: implemented signin flow with extension

--- website/src/hooks/browser.js
+++
@@ -1,8 +0,0 @@
-/* global chrome */
-import loadable from '@loadable/component';
-
-export default () => {
-  chrome.runtime.id = 'chummy-website';
-  const browser = loadable.lib(() => import('webextension-polyfill'));
-  return { browser };
-};
