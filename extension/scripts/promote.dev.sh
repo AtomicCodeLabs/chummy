2db5d76a9bc886c07c10e3d0d37c59e8d82b7677
Alex Kim <alexgkim205@gmail.com>
2020-12-29 20:38:20 -0500

    chore: don't fail commit if nothing to commit

--- extension/scripts/promote.dev.sh
+++ extension/scripts/promote.dev.sh
@@ -4,7 +4,7 @@ set -e
 # Checkout source branch first
 yarn checkout:dev
 git add -A
-HUSKY_SKIP_HOOKS=1 git commit -m "chore(pre-promote): push dev"
+git diff-index --quiet HEAD || HUSKY_SKIP_HOOKS=1 git commit -m "chore(pre-promote): push dev"
 git push -u origin extension/dev
 
 # Push backend
