c74a8687779b8915bca766049412858d6d296e02
Alex Kim <alexgkim205@gmail.com>
2021-02-12 14:16:22 -0800

    chore: fill in empty pages

---
+++ website/src/pages/checkout.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import Link from '../components/Link';
+import Layout from '../components/layout';
+import SEO from '../components/seo';
+
+const CheckoutPage = () => (
+  <Layout>
+    <SEO title="CheckoutPage" />
+    <h1>CheckoutPage</h1>
+    <Link to="/">Go home</Link>
+  </Layout>
+);
+
+export default CheckoutPage;
