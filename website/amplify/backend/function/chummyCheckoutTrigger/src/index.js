29320500d314fef13552f2964cde57e574db04b5
Alex Kim <alexgkim205@gmail.com>
2021-02-15 23:31:23 -0800

    chore: add new infra changes to prod

---
+++ website/amplify/backend/function/chummyCheckoutTrigger/src/index.js
@@ -0,0 +1,21 @@
+/* Amplify Params - DO NOT EDIT
+	API_CHUMMY_GRAPHQLAPIENDPOINTOUTPUT
+	API_CHUMMY_GRAPHQLAPIIDOUTPUT
+	API_CHUMMY_GRAPHQLAPIKEYOUTPUT
+	ENV
+	REGION
+Amplify Params - DO NOT EDIT */
+
+exports.handler = async (event) => {
+    // TODO implement
+    const response = {
+        statusCode: 200,
+    //  Uncomment below to enable CORS requests
+    //  headers: {
+    //      "Access-Control-Allow-Origin": "*",
+    //      "Access-Control-Allow-Headers": "*"
+    //  }, 
+        body: JSON.stringify('Hello from Lambda!'),
+    };
+    return response;
+};
