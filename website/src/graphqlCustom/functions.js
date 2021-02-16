6ecf272bee0be21ad50629bf720275d7043e551d
Alex Kim <alexgkim205@gmail.com>
2021-02-16 00:10:44 -0800

    fix: fix checkout for account pages

--- website/src/graphqlCustom/functions.js
+++ website/src/graphqlCustom/functions.js
@@ -1,14 +1,14 @@
 export const cancelSubscription = /* GraphQL */ `
-  mutation CancelSubscription($ddbId: String!) {
-    cancelSubscription(ddbId: $ddbId) {
+  mutation CancelSubscription {
+    cancelSubscription {
       id
     }
   }
 `;
 
 export const syncUser = /* GraphQL */ `
-  mutation SyncUser($ddbId: String!) {
-    syncUser(ddbId: $ddbId) {
+  mutation SyncUser {
+    syncUser {
       id
     }
   }
