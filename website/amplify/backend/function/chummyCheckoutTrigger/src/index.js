4ab039ee82091754de42e35f8f98353b001aaa13
Alex Kim <alexgkim205@gmail.com>
2021-02-15 23:27:00 -0800

    chore: resolve promotion to gamma

--- website/amplify/backend/function/chummyCheckoutTrigger/src/index.js
+++ website/amplify/backend/function/chummyCheckoutTrigger/src/index.js
@@ -6,16 +6,41 @@
 	REGION
 Amplify Params - DO NOT EDIT */
 
-exports.handler = async (event) => {
-    // TODO implement
-    const response = {
-        statusCode: 200,
-    //  Uncomment below to enable CORS requests
-    //  headers: {
-    //      "Access-Control-Allow-Origin": "*",
-    //      "Access-Control-Allow-Headers": "*"
-    //  }, 
-        body: JSON.stringify('Hello from Lambda!'),
-    };
-    return response;
+// Function resolvers
+// Multiple methods can be called in this lambda trigger
+const resolvers = {
+  Mutation: {
+    cancelSubscription: (event) => {
+      return cancelSubscription(event);
+    },
+    syncUser: (event) => {
+      return syncUser(event);
+    }
+  }
+};
+
+async function cancelSubscription(event) {
+  console.log('CANCEL SUBSCRIPTION', event);
+}
+
+async function syncUser(event) {
+  console.log('SYNC USER', event);
+}
+
+/**
+ * Processes a Stripe event triggered from the client
+ * @param {*} event
+ */
+exports.handler = async (event, context) => {
+  console.log('Event', event);
+  console.log('Context', context);
+
+  const typeHandler = resolvers[event.typeName];
+  if (typeHandler) {
+    const resolver = typeHandler[event.fieldName];
+    if (resolver) {
+      return await resolver(event);
+    }
+  }
+  throw new Error('Resolver not found.');
 };
