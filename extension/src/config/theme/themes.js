77a9823b381cb7923f80a05ede3e2278e15d3548
Alex Kim <alexgkim205@gmail.com>
2021-03-03 16:53:11 -0800

    feat: add janes theme and dracula

--- extension/src/config/theme/themes.js
+++ extension/src/config/theme/themes.js
@@ -4,3 +4,5 @@ export * as monokaiDark from '../../themes/monokai.dark.json';
 export * as materialLight from '../../themes/material.light.json';
 export * as materialDark from '../../themes/material.dark.json';
 export * as onedarkDark from '../../themes/onedark.dark.json';
+export * as janesThemeLight from '../../themes/janestheme.light.json';
+export * as draculaDark from '../../themes/dracula.dark.json';
