9fb734349ddba09600fe59cc5498a4e48d9451d2
Alex Kim <alexgkim205@gmail.com>
2021-03-16 17:12:57 -0700

    chore: add edge and opera webpack scripts

--- extension/scripts/publish.sh
+++ extension/scripts/publish.sh
@@ -7,8 +7,14 @@ pwd
 VERSION=$(cat ".version")
 STAGE=$1
 
-zip -r dist_$VERSION.web.zip web
-aws s3 cp dist_$VERSION.web.zip s3://chummy-assets-$STAGE/$VERSION/
+zip -r dist_$VERSION.chrome.zip chrome
+aws s3 cp dist_$VERSION.chrome.zip s3://chummy-assets-$STAGE/$VERSION/
+
+zip -r dist_$VERSION.edge.zip edge
+aws s3 cp dist_$VERSION.edge.zip s3://chummy-assets-$STAGE/$VERSION/
+
+zip -r dist_$VERSION.opera.zip edge
+aws s3 cp dist_$VERSION.opera.zip s3://chummy-assets-$STAGE/$VERSION/
 
 cd moz
 zip -r ../dist_$VERSION.moz.zip .
---
+++ extension/webpack/prod.opera.config.js
@@ -0,0 +1,33 @@
+const path = require('path');
+const CopyPlugin = require('copy-webpack-plugin');
+
+const base = require('./prod.web.config');
+const { formBaseManifest, generateReports } = require('./util');
+
+module.exports = {
+  ...base,
+  output: {
+    ...base.output,
+    path: path.join(__dirname, '../dist/opera'),
+    publicPath: '/'
+  },
+  plugins: [
+    ...base.plugins,
+    new CopyPlugin({
+      patterns: [
+        {
+          from: '../manifest-base.json',
+          to: './manifest.json',
+          transform(content) {
+            return JSON.stringify({
+              ...formBaseManifest(content)
+              // key:
+              //   'MIIBIjANBgkqhkiG9w0BAQEFAAOCAQ8AMIIBCgKCAQEAkZ7eURkxU+9PPkvVaVDUK88dZX39ZXKS9zRtpkAY6so1omoDZ6L3AWjy4e3ds8vz6OxeFcPzgycgTDVaPa2LAgvk2i+/eSbmFO8wvbp8Ce0/iPf2Vp0IqR1MQ+aRT+qD+6swNXvIJuAwFcuPP0LnDMe4veGVHyvI4uoelEVJ7P7RrnrskU4vscUAKHi5FygZLnfXzifrY2Vy6GA2wNipmd2I4+gW4ZnvSTzMs1u6s/k3LSg96cFxOl62AanEnuOcahUrCPl2/aTlU8OrOdgyiGvWKw4DxXsLC7XNZ589QvVP9uRdSsj7sAie/bGkTWRM3/NqYts8YhsMypWCCCxnQQIDAQAB'
+            });
+          }
+        }
+      ]
+    }),
+    ...generateReports('prod', 'opera')
+  ]
+};
